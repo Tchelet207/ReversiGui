9e45452b1bc67c0f53550fcdef77c6ba40e25aa4
User <tuts207@gmail.com>
2018-01-14 00:19:18 +0200

    start menue

---
+++ workspace/Reversi/src/game/Menue.java
@@ -0,0 +1,36 @@
+package game;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class Menue extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			VBox root = (VBox)FXMLLoader.load(getClass().getResource("MenuFxml.fxml"));
+			Scene scene = new Scene(root, 500, 500);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("Menue");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException exception) {
+		 	throw new RuntimeException(exception);
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
--- workspace/Reversi/src/game/Try.java
+++ workspace/Reversi/src/game/Try.java
@@ -2,8 +2,10 @@ package game;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -14,8 +16,13 @@ public class Try extends Application {
 		primaryStage.setTitle("My First App");
 		Label lbl = new Label("Hello World!");
 		lbl.setFont(new Font("Arial", 30));
-		StackPane root = new StackPane();
+		Button btn = new Button("Click me");
+		btn.setOnAction(event -> {
+		lbl.setText("Button clicked!");
+		});
+		VBox root = new VBox();
 		root.getChildren().add(lbl);
+		root.getChildren().add(btn);
 		primaryStage.setScene(new Scene(root, 300, 250));
 		primaryStage.show();
 	}
